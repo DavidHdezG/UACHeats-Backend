e03772261e26963c9b8c476a130db525f686619b
David <a338953@uach.mx>
2022-09-28 13:01:42 -0600

    1

---
+++ src/main/java/mx/uach/uacheats/UacheatsApplication.java
@@ -0,0 +1,13 @@
+package mx.uach.uacheats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UacheatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UacheatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/uach/uacheats/UacheatsApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.uach.uacheats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UacheatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
